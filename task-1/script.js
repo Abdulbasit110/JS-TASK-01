41d2dc27e9d6f54805cec0d4cdf83bde55f34fd0
ABDUL BASIT <BASIT4408944@CLOUD.NEDUET.EDU.PK>
2023-09-08 13:47:38 +0500

    Add files via upload

---
+++ task-1/script.js
@@ -0,0 +1,72 @@
+// --------------CHAPTER #01---------------
+
+// // Q1
+alert('welcome to our site');
+// // Q2
+alert('Error! Please enter a valid password');
+// // Q3
+alert('welcome to JS land.. \n Happy Codding!');
+// // Q4
+alert('welcome to JS land..');
+alert('Happy Codding! ');
+
+// // ShowAlert = () => {
+// //   window.alert('hello world');
+// // }
+
+// // Q5
+console.log(alert('hello.. I can run JS through my web browsers console'));
+
+
+// // --------------CHAPTER #02---------------
+
+// // Q1
+var username;
+
+// // Q2
+var myName ='abdul basit';
+
+// // Q3
+var message = 'hello world';
+alert(message);
+
+// Q4
+var std_name = `basit younas`;
+var std_age = 20;
+var CourseEnrolled = `Certified Mobile Application Developer`;
+alert(std_name);
+alert(std_age);
+alert(CourseEnrolled);
+
+// // Q5
+// var food = 'pizza';
+alert('pizza\npizz\npiz\npi\np');
+// x=food.length;
+// console.log(x);
+
+// for (let index = 0; index <  x; index++) {
+//   for (let y = 0; y < x-index; y++) {
+//      console.log(food);
+//   }
+// }
+// // while () {
+  
+// // }
+
+// // Q6
+var email = `basit4408944@cloud.neduet.edu.pk`;
+alert(`my email address is `+ email);
+
+// // Q7
+var book = `A smarter wayu to learn javascript`;
+alert('i am trying to learn form the book '+book);
+
+// // Q8
+var p  = document.getElementById('para');
+p.innerHTML= 'I can write HTML content through JS';
+p.style.color ="blue";
+
+// // Q9
+alert('“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ');
+
+// ---------END-----------
